cd0dca0cc4d47d7d3591fb8c324ac883a4ae793c
PRASANTH <154744075+PRASANTH682@users.noreply.github.com>
2025-01-24 11:46:54 +0530

    fix : change text

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 class hello{     
     public static void main(String[] args) {
-        System.out.println("This is Hello world...");
+        System.out.println("This is My first push opration");
     }
 }
